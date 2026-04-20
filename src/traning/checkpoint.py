9d59a67364e87fd0fcd14c8854f9ef3a55562bc0
HoangThang25 <hoangthang042586@gmail.com>
2026-04-21 00:14:16 +0700

    feat: implement checkpoint save_checkpoint/load_checkpoint

--- src/traning/checkpoint.py
+++ src/traning/checkpoint.py
@@ -1,5 +1,21 @@
+import torch
 def save_checkpoint(model, optimizer, epoch, best_metric, save_path):
-    pass
+    checkpoint = {
+        "model_state_dict": model.state_dict(),
+        "optimizer_state_dict": optimizer.state_dict(),
+        "epoch": epoch,
+        "best_metric": best_metric
+    }
+    
+    torch.save(checkpoint, save_path)
 
 def load_checkpoint(model, optimizer, checkpoint_path, device):
-    pass
+    checkpoint = torch.load(checkpoint_path, map_location=device)
+
+    model.load_state_dict(checkpoint["model_state_dict"])
+    optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+
+    epoch = checkpoint["epoch"]
+    best_metric = checkpoint["best_metric"]
+
+    return model, optimizer, epoch, best_metric

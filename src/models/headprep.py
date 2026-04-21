9cd3d50fef8b60c37e6536502e635cd91014110f
Mei-IwI <c2005vn@gmail.com>
2026-04-21 16:15:53 +0700

    resolve confict in headpred

--- src/models/headprep.py
+++ src/models/headprep.py
@@ -9,7 +9,6 @@ import torch.nn as nn
 
 class HeadPrep(nn.Module):
     def __init__(self):
-<<<<<<< HEAD
         super().__init__()
         self.flatten = nn.Flatten()
         self.fc1 = nn.Linear(256*14*14, 512)
@@ -19,19 +18,3 @@ class HeadPrep(nn.Module):
         x = self.flatten(x)
         x = self.fc1(x)
         x = self.relu(x)
-=======
-        super(HeadPrep, self).__init__()
-
-        self.flatten = nn.Flatten()
-        self.fc = nn.Linear(256 * 14 * 14, 512) 
-        self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(0.5)
-
-
-    def forward(self, x):
-        x = self.flatten(x)
-        x = self.fc(x)
-        x = self.relu(x)
-        x = self.dropout(x)
->>>>>>> origin/main
-        return x

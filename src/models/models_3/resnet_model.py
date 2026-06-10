02ef036b5a6ee695a00f1f0b354ffbecb0e23b54
Huhhuu <hokhachoaa13dt@gmail.com>
2026-06-10 11:21:31 +0700

    Cap nhat lai classifier_head cua model 3, bo sung BatchNorm, ReLU va Dropout

--- src/models/models_3/resnet_model.py
+++ src/models/models_3/resnet_model.py
@@ -4,13 +4,18 @@ from .classifier_head import ClassifierHead
 
 
 class ResNet18Classifier(nn.Module):
-    def __init__(self, num_classes: int, freeze_backbone=True, fine_tune=True):
+    def __init__(self, num_classes: int, freeze_backbone=False, fine_tune=False):
         super().__init__()
+
         self.backbone = ResNet18Backbone(
             freeze_backbone=freeze_backbone,
-            fine_tune=fine_tune,
+            fine_tune=fine_tune
         )
-        self.classifier = ClassifierHead(512, num_classes)
+
+        self.classifier = ClassifierHead(
+            in_features=512,
+            num_classes=num_classes
+        )
 
     def forward(self, x):
         x = self.backbone(x)

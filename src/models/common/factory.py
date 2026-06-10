3657bbed28a8c86118082cce0853d6535c32b4ed
Huhhuu <hokhachoaa13dt@gmail.com>
2026-06-10 11:11:47 +0700

    Cap nhat model 3

--- src/models/common/factory.py
+++ src/models/common/factory.py
@@ -1,6 +1,6 @@
 from src.models.models_1.model import build_model_1
 from src.models.models_2.model import build_model_2
-# from src.models.models_3.model import build_model_3
+from src.models.models_3.model import build_model_3
 from src.models.models_4.model import build_model_4
 
 
@@ -9,10 +9,8 @@ def build_model(model_name: str, n_classes: int, device: str):
         return build_model_1(n_classes=n_classes, device=device)
     elif model_name == "model_2":
         return build_model_2(n_classes=n_classes, device=device)
-        pass
     elif model_name == "model_3":
-        # return build_model_3(n_classes=n_classes, device=device)
-        pass
+        return build_model_3(n_classes=n_classes, device=device)
     elif model_name == "model_4":
         return build_model_4(n_classes=n_classes, device=device)
     else:
